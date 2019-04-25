9b6890323f4c0bc5c9bf1e9da56a7ca398f52102
Geanie Blanco <geanieblanco@gmail.com>
2019-04-25 10:39:04 -0400

    clean local

---
+++ Project/AwayMessage/AwayMessageTests/AwayMessageTests.swift
@@ -0,0 +1,34 @@
+//
+//  AwayMessageTests.swift
+//  AwayMessageTests
+//
+//  Created by Geanie Blanco on 12/16/18.
+//  Copyright © 2018 Geanie Blanco. All rights reserved.
+//
+
+import XCTest
+@testable import AwayMessage
+
+class AwayMessageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
